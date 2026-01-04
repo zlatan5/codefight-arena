83d7878f6d5ed2c96be4e49c572d75556fcf0d91
Amit Ranjan <your-email@example.com>
2026-01-04 10:16:42 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/com/example/code_fight_arena/CodeFightArenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.code_fight_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFightArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFightArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/code_fight_arena/CodeFightArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_fight_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFightArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
